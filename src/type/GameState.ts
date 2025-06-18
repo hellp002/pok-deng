8db4ea1433674e009adeb9b46010e3062012d05c
Werapat <hellp002@gmail.com>
2025-06-18 16:23:50 +0700

    feat : ad game state

--- src/type/GameState.ts
+++ src/type/GameState.ts
@@ -1,1 +1,2 @@
-export type GameState = "WIN" | "LOSE" | "TIE";
+export type GameState = "PUT_BET" | "DEAL_CARDS" | "PLAYER_TURN" | "DEALER_TURN" | "CHECK_WINNER" | "RESTARTABLE" | "END_GAME";
+export type GameResult = "PLAYER_WIN" | "DEALER_WIN" | "TIE" ;
